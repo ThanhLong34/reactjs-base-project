4b0310081cf29ebc3834a564b5ccd132e2f76b5e
DragonDev <thanhlongedu0304@gmail.com>
2023-03-29 20:43:33 +0700

    Setup base

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Styles
+import './assets/styles/reset.scss';
+import './assets/styles/main.scss';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
